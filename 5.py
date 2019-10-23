c9587c48c7c4eed4d58e89433bce53ee7ba39a6d
sharmaji07 <33605535+sharmaji07@users.noreply.github.com>
2019-10-23 22:13:59 +0530

    q 2,3,4
    checked

---
+++ 5.py
@@ -0,0 +1,9 @@
+#HOW WILL U ACCESS ELEMENTS IN A LIST OF DIMENSIONS 4X3
+
+a= [[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]]
+print(a)
+for i in range(0,4):
+    for j in range(0,3):
+        print(a[i][j])
+
+
